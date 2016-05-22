1e7e6955ef886f904d630577571afbae63082079
jrtechs <jeffery@jrtechs.net>
2016-05-22 18:22:13 -0400

    creation of the living class

---
+++ src/tanks/Living.java
@@ -0,0 +1,13 @@
+/*
+    super class for any object that is alive
+    has health, and a boolean isAlive
+    every living element can take damage from another Rotational Element
+    determines damage by using instanceof
+*/
+package tanks;
+
+
+public class Living extends RotationalElement
+{
+    
+}
