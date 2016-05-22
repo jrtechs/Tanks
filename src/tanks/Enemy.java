9c90c7cd089b0094043c65ddb88499ff014483a5
jrtechs <jeffery@jrtechs.net>
2016-05-22 18:24:31 -0400

    creation of the Enemy class

---
+++ src/tanks/Enemy.java
@@ -0,0 +1,12 @@
+/*
+    super class for any enemy in the game
+    a enemy can spawn at a random location along the border of the map
+    a enemy can also calculate the distance and angle to the player
+*/
+package tanks;
+
+
+public class Enemy extends Living
+{
+    
+}
